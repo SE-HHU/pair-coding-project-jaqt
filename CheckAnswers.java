a752e6e856607b8e061344ca2df975df3b21ecdd
FishMoun <89787263+FishMoun@users.noreply.github.com>
2021-10-05 10:05:55 +0800

    Add files via upload

--- CheckAnswers.java
+++ CheckAnswers.java
@@ -15,11 +15,11 @@ public class CheckAnswers
      * Calls:          //无
      * Calls By:       //produceFile()
      */
-    void checkCorrectness(String path1,String path2,String path3) throws Exception
+    void checkCorrectness() throws Exception
     {
         FileWriter file3;
-        File file1 =new File(path1);
-        File file2 =new File(path2);
+        File file1 =new File("Exercises.txt");
+        File file2 =new File("Answers.txt");
         InputStreamReader read1 = new InputStreamReader(new FileInputStream(file1));
         BufferedReader f1=new BufferedReader(read1);
         InputStreamReader read2 = new InputStreamReader(new FileInputStream(file2));
@@ -48,7 +48,7 @@ public class CheckAnswers
             }
             i++;
         }
-        file3 = new FileWriter(path3, true);
+        file3 = new FileWriter("Grade.txt", true);
         file3.write("Correct:"+correct+"(");
         for (int p=0;p<correct;p++)
         {
@@ -84,69 +84,66 @@ public class CheckAnswers
      * Calls:          //无
      * Calls By:       //produceFile()
      */
-    void checkRepeat (String path1,String path2,String path3) throws Exception
+    void checkRepeat() throws Exception
     {
         FileWriter file3;
-        file3 = new FileWriter(path3, true);
-        File file1 =new File(path1);
-        File file2 =new File(path2);
+        file3 = new FileWriter("Grade.txt", true);
+        File file1 =new File("Exercises.txt");//读取题目文件
+        File file2 =new File("Answers.txt");//读取答案文件
         InputStreamReader read1 = new InputStreamReader(new FileInputStream(file1));//题目文件
         BufferedReader f1=new BufferedReader(read1);
         InputStreamReader read2 = new InputStreamReader(new FileInputStream(file2));//答案文件
         BufferedReader f2=new BufferedReader(read2);
 
-
-        String line1;
-        String line2;
-
-        int m=1;int p=1;
-        int i=0;
+        int m=1;int p=1;// m,p分别为答案和题目的题号
+        int i=0;//i为ll[]参数，看有多少重复的
         int repeat=0;
 
         String [][] line3 = new String [10000][];//存放答案
         String [][] line4 = new String [10000][];//存放题目文件后缀表达式的各个元素
         String [] line5 = new String [10000];//存放所有题目，用于打印重复题目
         int [] ll=new int[10000];//存放重复题目的题号
+        String line1;
+        String line2;
         StackCalculate a = new StackCalculate(numRange);
-        while((line1=f1.readLine())!=null) {
-            line4[p]=a.toPostfix(line1).split(" ");
-            line5[p]=line1;
+        while((line1=f1.readLine())!=null) {   //line1读取题目文件
+            line4[p]=a.toPostfix(line1).split(" ");//将题目数字和符号分开并以后缀表达式的格式一个一个存入line4[p]中
+            line5[p]=line1;//将原题目存入line5[p]中，一旦重复，便于打印
             p++;
         }
 
         while ((line2=f2.readLine())!=null){
-            line3[m]=a.toPostfix(line2).split(" ");
+            line3[m]=a.toPostfix(line2).split(" ");//存放答案，line3[m][0]为题号，line3[m][1]为答案 如果为分数就是分子位
             m++;
         }
 
-        for(int n=1;n<m;n++)
+        for(int n=1;n<m;n++) //循环嵌套判断答案是否相等
         {
             for(int k=n+1;k<m;k++)
             {
 
-                if(line3[k][1].equals(line3[n][1]))//第n题和第k题答案相同时  判断后缀表达式的元素是否相同；
+                if(line3[k][1].equals(line3[n][1]))//第n题和第k题答案或者分子位相同时  判断后缀表达式的元素是否相同；
                 {
-                    if(line4[k].length==line4[n].length)
+                    if(line4[k].length==line4[n].length)//判断后缀表达式的长度是否一样，不一样就省去比较，一样在比较
                     {
                         int u=0;
-                        // System.out.println("u的值为"+u);
                         for(int q=1;q<line4[n].length;q++)
                         {
 
                             for(int c=1;c<line4[k].length;c++)
                             {
-                                if(line4[n][q].equals(line4[k][c])) {
+                                if(line4[n][q].equals(line4[k][c])) {//循环判断是否一个后缀表达式与另外一个后缀表达式有相同元素，如果有，就直接跳出再循环
                                     u++;
                                     break;
                                 }
                             }
                         }
-                        if(u==line4[n].length-1)
+                        if(u==line4[n].length-1)//
                         {
                             repeat++;
-                            ll[i]=n;
+                            ll[i]=n;//用ll[i]存放题目编号,便于用line5[]打印题目
                             ll[i+1]=k;
-                            i+=2;
+                            i+=2;//一次存放两个，存放完i+=2
                         }
                     }
                 }
@@ -156,11 +153,11 @@ public class CheckAnswers
         file3.write("\n");
         file3.write("RepeatDetail:");
         file3.write("\n");
-        int ee=0;//ee为重复题目的题号-1
+        int re=0;//re为重复题目的题号-1
         for(int e=0;e<repeat;e++)
         {
-            file3.write("("+(e+1)+")"+" "+line5[ll[ee]]+" Repeat"+" "+line5[ll[ee+1]]);
-            ee+=2;
+            file3.write("("+(e+1)+")"+" "+line5[ll[re]]+" Repeat"+" "+line5[ll[re+1]]);
+            re+=2;
             file3.write("\n");
         }
         file3.flush();
