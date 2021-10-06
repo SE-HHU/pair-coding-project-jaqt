5b7b0e88962976c3b2ece11f65c27f4acc1b41c6
FishMoun <89787263+FishMoun@users.noreply.github.com>
2021-10-06 19:52:59 +0800

    Add files via upload

--- StackCalculate.java
+++ StackCalculate.java
@@ -11,27 +11,36 @@ public class StackCalculate
     {
         this.numRange = numRange;
     }
+    public StackCalculate()
+    {
+
+    }
     /*
      * function:       //getResult
      * Description:    //调用函数先将一般式转化为后缀表达式，再计算获取结果
      * Calls:          //toPostfix
      * Calls By:       //getResult
      */
-    String getResult(String str) {
-        //变成后缀表达式
-        try {
-            str = toPostfix(str);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    String getResult(String str)
+    {
         if (str.contains("/0"))
             return "-1";
+        //变成后缀表达式
+        try
+        {
+            str = toPostfix(str);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         String[] str0 = str.split(" ");
         if (!str.contains("/")) //不含除号的基本运算
         {
             Stack<Integer> stack = new Stack<>();
             for (String c : str0) {
-                if (c.equals("+") || c.equals("-") || c.equals("*")) {
+                if (c.equals("+") || c.equals("-") || c.equals("*"))
+                {
                     int b = stack.peek();
                     stack.pop();
                     int a = stack.peek();
@@ -42,17 +51,21 @@ public class StackCalculate
                     //在范围之外
                     if (a < 0 || a > numRange || b < 0 || b > numRange)
                         return "-1";
-                } else
+                }
+                else
                     stack.push(Integer.parseInt(c));
             }
             if (stack.peek() < 0 || stack.peek() > numRange)
                 return "-1";
             return Integer.toString(stack.peek());
-        } else //带除号转换成分数运算
+        }
+        else //带除号转换成分数运算
         {
             Stack<int[]> stack = new Stack<>();
-            for (String c : str0) {
-                if (c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")) {
+            for (String c : str0)
+            {
+                if (c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/"))
+                {
                     int[] b = stack.peek();
                     stack.pop();
                     int[] a = stack.peek();
@@ -62,17 +75,16 @@ public class StackCalculate
                             || a[0] > a[1] && a[1] != 1 || b[0] > b[1] && b[1] != 1)
                         return "-1";
                     stack.push(calculate(a[0], a[1], b[0], b[1], c));
-                } else
+                }
+                else
                     stack.push(new int[]{Integer.parseInt(c), 1});
             }
             //继续对结论进行考察
+            if (stack.peek()[1] == 1)
+                return Integer.toString(stack.peek()[0]);
             if (stack.peek()[0] < 0 || stack.peek()[0] > numRange || stack.peek()[0] > stack.peek()[1]
                     || stack.peek()[1] < 0 || stack.peek()[1] > numRange)
                 return "-1";
-            if (stack.peek()[0] == stack.peek()[1])
-                return "1";
-            else if (stack.peek()[1] == 1)
-                return Integer.toString(stack.peek()[0]);
             return stack.peek()[0] + "/" + stack.peek()[1];
         }
     }
@@ -82,12 +94,14 @@ public class StackCalculate
      * Calls:          //无
      * Calls By:       //getResult()
      */
-    String toPostfix(String str) throws Exception {
+    String toPostfix(String str) throws Exception
+    {
         Stack<String> postfix = new Stack<>();
         StringBuilder postFix = new StringBuilder();
         Pattern p = Pattern.compile("(?<!\\d)-?\\d+(\\.\\d+)?|[+\\-*/()]");//这个正则为匹配表达式中的数字或运算符
         Matcher m = p.matcher(str);
-        while (m.find()) {
+        while (m.find())
+        {
             String c = m.group();
             if (c.equals("("))//遇到左括号，直接压栈
             {
@@ -96,25 +110,31 @@ public class StackCalculate
             }
             if (c.equals(")"))//遇到右括号，弹栈输出直到弹出左括号（左括号不输出）
             {
-                while (!postfix.peek().equals("(")) {
+                while (!postfix.peek().equals("("))
+                {
                     postFix.append(postfix.peek() + " ");
                     postfix.pop();
                 }
                 postfix.pop();
                 continue;
             }
-            if (c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")) {
-                if (!postfix.empty() && getPriority(postfix.peek()) >= getPriority(c)) {
-                    while (!postfix.empty() && getPriority(postfix.peek()) >= getPriority(c)) {
+            if (c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/"))
+            {
+                if (!postfix.empty() && getPriority(postfix.peek()) >= getPriority(c))
+                {
+                    while (!postfix.empty() && getPriority(postfix.peek()) >= getPriority(c))
+                    {
                         postFix.append(postfix.peek() + " ");
                         postfix.pop();
                     }
                 }
                 postfix.push(c);
-            } else
+            }
+            else
                 postFix.append(c + " ");
         }
-        while (!postfix.empty()) {
+        while (!postfix.empty())
+        {
             postFix.append(postfix.peek() + " ");
             postfix.pop();
         }
@@ -122,7 +142,8 @@ public class StackCalculate
     }
 
     //获得优先级
-    int getPriority(String ch) throws Exception {
+    int getPriority(String ch) throws Exception
+    {
         if (ch.equals("+") || ch.equals("-"))
             return 1;
         if (ch.equals("*") || ch.equals("/"))
@@ -133,7 +154,8 @@ public class StackCalculate
     }
 
     //整数运算
-    int calculate(int a, int b, String ch) {
+    int calculate(int a, int b, String ch)
+    {
         if (ch.equals("+"))
             return a + b;
         if (ch.equals("-"))
@@ -144,7 +166,8 @@ public class StackCalculate
     }
 
     //分数运算
-    int[] calculate(int a1, int a2, int b1, int b2, String ch) {
+    int[] calculate(int a1, int a2, int b1, int b2, String ch)
+    {
         FractionFormat a = new FractionFormat(a1, a2);
         FractionFormat b = new FractionFormat(b1, b2);
         if (ch.equals("+"))
